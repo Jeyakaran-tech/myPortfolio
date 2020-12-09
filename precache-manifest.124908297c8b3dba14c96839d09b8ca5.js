9ea7bf90e2cdbc70a70f64d8632302388050ef3a
Jeyakaran karnan <Jeyakaran Karnan>
2020-12-10 01:06:22 +1100

    Updates

---
+++ precache-manifest.124908297c8b3dba14c96839d09b8ca5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2eeef6543b180436fc3f526f4e13382",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "b48b9a71808d2c579430",
+    "url": "/myPortfolio/static/css/2.f1b50ea8.chunk.css"
+  },
+  {
+    "revision": "edfb5551948364b40748",
+    "url": "/myPortfolio/static/css/main.27cc4aa5.chunk.css"
+  },
+  {
+    "revision": "b48b9a71808d2c579430",
+    "url": "/myPortfolio/static/js/2.f93aa971.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myPortfolio/static/js/2.f93aa971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfb5551948364b40748",
+    "url": "/myPortfolio/static/js/main.9630fd41.chunk.js"
+  },
+  {
+    "revision": "8af9ec56f27e437708a4",
+    "url": "/myPortfolio/static/js/runtime-main.975ec84c.js"
+  },
+  {
+    "revision": "2aa3677d8b5a56c8465dbb865024ce43",
+    "url": "/myPortfolio/static/media/Amazon.2aa3677d.png"
+  },
+  {
+    "revision": "30b093495cb87e6bc6dc4b88c8351e29",
+    "url": "/myPortfolio/static/media/CSS.30b09349.png"
+  },
+  {
+    "revision": "bb0f5db81a42804de83522a9f5ee6532",
+    "url": "/myPortfolio/static/media/EMR.bb0f5db8.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/myPortfolio/static/media/HTML.f2282f94.png"
+  },
+  {
+    "revision": "271016d937f6eab4d3c2e02aeb10f039",
+    "url": "/myPortfolio/static/media/R.271016d9.png"
+  },
+  {
+    "revision": "e06f5e1d06898713be682759fb110472",
+    "url": "/myPortfolio/static/media/android.e06f5e1d.jpeg"
+  },
+  {
+    "revision": "9d736f9730e70b638a7cca58327af976",
+    "url": "/myPortfolio/static/media/arduino.9d736f97.png"
+  },
+  {
+    "revision": "2df50bf0be6c5ebe6960fe91cf9ed741",
+    "url": "/myPortfolio/static/media/bootstrap.2df50bf0.png"
+  },
+  {
+    "revision": "7f3f295cc9b901f58165cb11ed7bc3a8",
+    "url": "/myPortfolio/static/media/ctslogo.7f3f295c.png"
+  },
+  {
+    "revision": "6e726ba1d7b3cd6c0c7d7c78cc006125",
+    "url": "/myPortfolio/static/media/dplyr.6e726ba1.png"
+  },
+  {
+    "revision": "d22043ec1c12d0468b56de278322d019",
+    "url": "/myPortfolio/static/media/java.d22043ec.png"
+  },
+  {
+    "revision": "e4b2223952c2dd668683a93f61183549",
+    "url": "/myPortfolio/static/media/javafx.e4b22239.jpg"
+  },
+  {
+    "revision": "031395272d048b4d2d64ebd5235872d3",
+    "url": "/myPortfolio/static/media/logo1.03139527.png"
+  },
+  {
+    "revision": "637d258f88159d54b623be86100676e8",
+    "url": "/myPortfolio/static/media/numpy.637d258f.png"
+  },
+  {
+    "revision": "3e24eae8fad8a41694ab1cd447fb2bb2",
+    "url": "/myPortfolio/static/media/pandas.3e24eae8.png"
+  },
+  {
+    "revision": "b2de173032ea0f6f0a998fb47c96daca",
+    "url": "/myPortfolio/static/media/profile.b2de1730.jpg"
+  },
+  {
+    "revision": "dce2d7caa795abd627854366dc31594f",
+    "url": "/myPortfolio/static/media/readr.dce2d7ca.png"
+  },
+  {
+    "revision": "c1d58c387fb4097c4d2721e208cbb115",
+    "url": "/myPortfolio/static/media/scene_builder.c1d58c38.png"
+  },
+  {
+    "revision": "5ae84fc13297e55e9efa90d4945ca074",
+    "url": "/myPortfolio/static/media/seaborn.5ae84fc1.png"
+  }
+]);
