ea1fe41a1034721c1a595b90dce6dd6bf3b3efa7
Jeyakaran karnan <Jeyakaran Karnan>
2020-12-10 20:28:49 +1100

    final

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -23,28 +23,28 @@ const About = () => {
               <Row className="d-flex justify-content-center flex-wrap">
               <div>
                     <a href="#contact">
-                      <Button className="m-2" variant="outline-primary">
+                      <Button variant="dark" className="m-2" >
                         Let's talk
                       </Button>
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1CfMY4NM1G7gFlmYXcK9_o554vHCdAVQn/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                      <Button className="m-2" variant="outline-success">
-                        My Resume
+                    <a href="https://drive.google.com/file/d/1nB_4BWEx2SuVpJ-tiuEksApk1Zhqp6gC/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button variant="dark" className="m-2" >
+                        My Résumé
                       </Button>
                     </a>
                   </div>
                   <div>
                     <a href="https://github.com/Jeyakaran-tech" target="_blank" rel="noopener noreferrer">
-                      <Button className="m-2" variant="outline-dark">
+                      <Button className="m-2" variant="dark">
                         GitHub
                       </Button>
                     </a>
                   </div>
                   <div>
                     <a href="https://www.linkedin.com/in/jeyakaran-karnan-a5a77a126/" target="_blank" rel="noopener noreferrer">
-                      <Button className="m-2" variant="outline-info">
+                      <Button className="m-2" variant="dark">
                         LinkedIn
                       </Button>
                     </a>
@@ -57,15 +57,17 @@ const About = () => {
                
                 Hi! I am <strong>&nbsp;Jeyakaran Karnan</strong>
                 <p>
-                <br />A passionate programmer, from India who successfully completed Under-graduation in Engineering with specialization in <strong>'Information Technology' in 2017.</strong> I love learning and update myself 
-                in technology, which is changing day to day.
+                <br />An avid programmer with the love and urge for untagling complex fixtures through any form of code. You've got a tough problem? - <strong>You've got me :)</strong>
+
                 <br />
-                After which I worked in a Multi National Corporate Company, which went really well. Working with Clients and amazing leads in my company, 
-                I learnt delivering the products with high efficiency and accuracy. After two years, I 
-                really wanted to shape my career deep in <strong>Data Science.</strong>  
-                <br/>After which, I came to Australia to pursue my <strong>Masters in Data Science.</strong> With all my power in knowledge gained so far and with my ambitious interest in learning and update myself, 
-                I am Seeking to leverage the data solutions to the maximum to solve the different business problems which results in the welfare of the Society.
-                 <br />
+                Having built up a strong base of coding through my bachelor of technology in IT, I'm on the climb to constant updation and improvement. 
+                I joined the workforce in 2017 December at <strong>Cognizant</strong>, through which i was able to be an aid to the team of developers as a programing analyst. 
+I was able to exert my knowledge and practice for the benefit of the healthcare sector services of Cognizant and completely fullfil the requirements
+of the clients.
+<br />
+Apart from working along for an MNC, I was also able to learn to overcome all forms of programming hurdles. 
+The two years of employment taught me a great deal of personal values, professional values and most importantly stregthened domain knowledge. 
+               <br />
                  </p>
     
               </Row>
